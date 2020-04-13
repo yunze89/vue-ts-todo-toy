2256f645556fc0b7346fb6f8cac6169e4db5d76f
Yunsuk Hong <yunze89@gmail.com>
2020-04-13 21:25:52 +0900

    bootstrap 추가, Todolist component 구현 중

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <todo-list></todo-list>
+<!--    <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view />-->
   </div>
 </template>
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  import TodoList from "@/components/TodoList.vue";
+
+  @Component({
+    components: {
+      TodoList
+    }
+  })
+  export default class App extends Vue{}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="todolist">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class TodoList extends Vue {
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
